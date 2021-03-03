510819b5a6285d724852cbddd986033265ab3a3f
Marcin Sarna <marcin@sarna.info>
2021-03-03 15:57:08 +0100

    updatePosition

--- src/main/java/com/autosledz/backend/config/CoreConfiguration.java
+++ src/main/java/com/autosledz/backend/config/CoreConfiguration.java
@@ -1,5 +1,6 @@
 package com.autosledz.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,4 +11,7 @@ public class CoreConfiguration {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Value("${admin.name}")
+    private String adminName;
 }
--- src/main/java/com/autosledz/backend/service/CarTrackService.java
+++ src/main/java/com/autosledz/backend/service/CarTrackService.java
@@ -2,9 +2,9 @@ package com.autosledz.backend.service;
 
 import com.autosledz.backend.controller.DeviceNotFoundException;
 import com.autosledz.backend.controller.TraccarController;
-import com.autosledz.backend.domain.Device;
 import com.autosledz.backend.domain.DeviceDto;
 import com.autosledz.backend.domain.GeocodingDto;
+import com.autosledz.backend.domain.traccar.TraccarDeviceDto;
 import com.autosledz.backend.domain.traccar.TraccarPositionDto;
 import com.autosledz.backend.mapper.CarTrackMapper;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -26,16 +26,17 @@ public class CarTrackService {
     public DeviceDto updatePositionOfDevice(Long deviceId) throws DeviceNotFoundException {
         List<TraccarPositionDto> listOfPositions = traccarController.getTraccarPositions();
         for(TraccarPositionDto currentPosition : listOfPositions) {
-            Optional<Device> device = service.getDevice(deviceId);
-            if(currentPosition.getDeviceId() == Long.parseLong(device.get().getUniqueId())) {
+            List<TraccarDeviceDto> traccarDevices = traccarController.getTraccarDevices();
+            List<TraccarDeviceDto> traccarDevice = traccarDevices.stream()
+                    .filter(d -> d.getId() == deviceId)
+                    .collect(Collectors.toList());
+            if(currentPosition.getDeviceId().equals(traccarDevice.get(0).getId())) {
                 Float currentPositionLatitude = currentPosition.getLatitude();
                 Float currentPositionLongitude = currentPosition.getLongitude();
                 GeocodingDto currentPositionGeocoding = geocodingService.getGeocoding(currentPositionLatitude, currentPositionLongitude);
-                if(device.isPresent()) {
-                    DeviceDto deviceWithUpdatedPosition = new DeviceDto(deviceId, service.getDevice(deviceId).get().getName(), device.get().getUniqueId(), currentPositionLatitude, currentPositionLongitude, currentPositionGeocoding.getDisplayName(), service.getDevice(deviceId).get().getCreated(), new Date());
-                    service.saveDevice(carTrackMapper.mapToDevice(deviceWithUpdatedPosition));
-                    return deviceWithUpdatedPosition;
-                }
+                DeviceDto deviceWithUpdatedPosition = new DeviceDto(deviceId, service.getDevice(deviceId).get().getName(), service.getDevice(deviceId).get().getUniqueId(), currentPositionLatitude, currentPositionLongitude, currentPositionGeocoding.getDisplayName(), service.getDevice(deviceId).get().getCreated(), new Date());
+                service.saveDevice(carTrackMapper.mapToDevice(deviceWithUpdatedPosition));
+                return deviceWithUpdatedPosition;
             }
         }
         throw new DeviceNotFoundException();
